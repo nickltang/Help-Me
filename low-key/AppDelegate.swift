eac2a9739b0e60efcda31139a90eb7dc42d6520a
Eunice Oh <euniceyoh@gmail.com>
2020-10-30 19:50:38 -0700

    new xcode files

---
+++ low-key/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  low-key
+//
+//  Created by Eunice Oh on 10/30/20.
+//  Copyright © 2020 Eunice Oh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
