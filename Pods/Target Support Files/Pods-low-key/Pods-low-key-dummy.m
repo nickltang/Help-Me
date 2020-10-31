7ead0913bf3157f743f2dbf74aa76b8ef90d9522
Eunice Oh <euniceyoh@gmail.com>
2020-10-30 20:24:53 -0700

    adding ios app files with firebase

---
+++ Pods/Target Support Files/Pods-low-key/Pods-low-key-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_low_key : NSObject
+@end
+@implementation PodsDummy_Pods_low_key
+@end
---
+++ low-key/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  low-key
+//
+//  Created by Eunice Oh on 10/30/20.
+//  Copyright © 2020 Eunice Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
