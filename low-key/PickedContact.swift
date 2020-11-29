96314ac50eb9aeed183a2cff2d62b143a84226b4
Eunice Oh <euniceyoh@gmail.com>
2020-11-28 19:35:34 -0800

    partial contacts set up

---
+++ low-key/PickedContact.swift
@@ -0,0 +1,16 @@
+//
+//  PickedContact.swift
+//  low-key
+//
+//  Created by Eunice Oh on 11/28/20.
+//  Copyright © 2020 Eunice Oh. All rights reserved.
+//
+
+import Foundation
+import Contacts
+
+struct PickedContact {
+    var firstName: String
+    var lastName: String
+    var telephone: String
+}
