4bf42d005527ca81683020182b2594502fa98d27
Eunice Oh <euniceyoh@gmail.com>
2020-12-11 13:26:20 -0800

    added picker with different moods

--- low-key/View Controllers/StatusViewController.swift
+++ low-key/View Controllers/StatusViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class StatusViewController: UIViewController {
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var moodLabel: UILabel!
+    @IBOutlet weak var picker: UIPickerView!
+    let moodData = ["Sad", "Happy", "Mad", "Pretty Savage", "≧◡≦", "(☆ᴗ☆ ◍)", "(⋆////⋆)", "( ͡👁️ ͜ʖ ͡👁️)", "(ㆆ_ㆆ)", "ↁ_ↁ", "(>.<)"]
+        
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+//        // connects the data
+        picker.delegate = self
+        picker.dataSource = self
     }
 
+}
+
+extension StatusViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return moodData.count
+    }
+    
+    
+}
+
+extension StatusViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return moodData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        // use the row to get the selected row from the picker view
+        // using the row extract the value from your datasource (array[row])
+        moodLabel.text = moodData[row]
+    }
 }
