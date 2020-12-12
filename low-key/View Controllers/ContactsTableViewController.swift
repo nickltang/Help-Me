1d1ff2affb9923fa5416db3bea4db08f2b0043ae
Eunice Oh <euniceyoh@gmail.com>
2020-12-11 23:59:41 -0800

    message

--- low-key/View Controllers/ContactsTableViewController.swift
+++ low-key/View Controllers/ContactsTableViewController.swift
@@ -20,7 +20,7 @@ class ContactsTableViewController: UITableViewController, CNContactPickerDelegat
     
     var AccoutSid = "AC4c54ed426d8623588135df369aa2020e"
     //ProcessInfo.processInfo.environment["TWILIO_ACCOUNT_SID"]
-    var AuthToken = "a6cb9de3e3d9166d2cf9ce5e47b66f73"
+    var AuthToken = "babda9a14e6d978a065f5d633b898cc6"
     //ProcessInfo.processInfo.environment["TWILIO_AUTH_TOKEN"]
     var twilioNumber = "+13158430633" // change to Alphanumeric ID "low-key"
     
@@ -156,57 +156,3 @@ class ContactsTableViewController: UITableViewController, CNContactPickerDelegat
     }
 }
 
-/*
-override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-    // Configure the cell...
-
-    return cell
-}
-*/
-
-/*
-// Override to support conditional editing of the table view.
-override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-}
-*/
-
-/*
-// Override to support editing the table view.
-override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-    if editingStyle == .delete {
-        // Delete the row from the data source
-        tableView.deleteRows(at: [indexPath], with: .fade)
-    } else if editingStyle == .insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-    // Return false if you do not want the item to be re-orderable.
-    return true
-}
-*/
-
-/*
-// MARK: - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    // Get the new view controller using segue.destination.
-    // Pass the selected object to the new view controller.
-}
-*/
